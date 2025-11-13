39d3737b301ef7e087c3096c4793190f7697faad
Rakib Hossain <rizviofficialteam@gmail.com>
2025-11-13 18:40:18 +0600

    .env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9ifWA1jqUhllCNcmDuDdJHPCek2fQBOU",
-  authDomain: "movies-master-pro.firebaseapp.com",
-  projectId: "movies-master-pro",
-  storageBucket: "movies-master-pro.firebasestorage.app",
-  messagingSenderId: "731543607196",
-  appId: "1:731543607196:web:139805e655847161511a59",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
